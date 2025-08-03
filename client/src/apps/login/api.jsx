0c2ce83c295051e7cd2befef19f4658ce02b0c6e
Jeevan E <jeevandevi610@gmail.com>
2025-08-03 18:09:34 +0530

    login deployment

--- client/src/apps/login/api.jsx
+++ client/src/apps/login/api.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5004', // your backend URL
+  baseURL: 'https://cybernaut-lms-login.onrender.com', // your backend URL
 });
 
 // Add token to headers
