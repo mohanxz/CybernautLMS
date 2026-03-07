b4852cd919a42a1c35fb37e0071a61d3bba1ed44
mohanxz <mohanrajs2k04@gmail.com>
2026-03-07 22:02:04 +0530

    LMS pushed in github

--- loginserver/backend/models/Course.js
+++ loginserver/backend/models/Course.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const courseSchema = new mongoose.Schema({
-  courseName: { type: String, required: true },
-  modules: [{ type: String, required: true }]
-}, { timestamps: true });
+const courseSchema = new mongoose.Schema(
+  {
+    courseName: { type: String, required: true },
+    modules: [{ type: String, required: true }],
+  },
+  { timestamps: true },
+);
 
-module.exports = mongoose.model('Course', courseSchema);
-
+module.exports = mongoose.model("Course", courseSchema);
